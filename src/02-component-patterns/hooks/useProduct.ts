9df55817be34bb424d1611f4a507c6c01339fb68
AleTartagliaNavent <atartaglia@navent.com>
2022-09-12 16:21:36 -0300

    subo component control terminado

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,34 +1,39 @@
 import { useEffect, useRef, useState } from 'react'
-import { OnChangeArgs, Product } from '../interfaces/interfaces';
+import { onChangeArgs, Product } from '../interfaces/interfaces';
+
 
 interface useProductArgs {
   product: Product;
-  onChange?: (args: OnChangeArgs) => void
-  value?: number
+  onChange?: (args: onChangeArgs) => void;
+  value?: number;
 }
 
 
 export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
 
-  const [counter, setCounter] = useState(value)
+  const [counter, setCounter] = useState(value);
+
   const isControlled = useRef(!!onChange)
 
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ count: value, product })
-    }
-    const newValue = Math.max(counter + value, 0)
-    setCounter(newValue)
 
-    onChange && onChange({ count: newValue, product })
+    if (isControlled.current) {
+      return onChange!({ count: value, product });
+    }
+
+    const newValue = Math.max(counter + value, 0)
+    setCounter(newValue);
+
+    onChange && onChange({ count: newValue, product });
   }
 
   useEffect(() => {
-    setCounter(value)
-
-
+    setCounter(value);
   }, [value])
 
+  return {
+    counter,
+    increaseBy
+  }
 
-  return { counter, increaseBy }
 }
